bf8c7a59cdec01988830e200660d0b835e8f4500
vedvyas9 <vedvyas9@gmail.com>
2020-11-23 15:51:30 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyttsx3
+import PyPDF2
+
+book=open('management.pdf','rb')
+pdfReader=PyPDF2.PdfFileReader(book)
+pages=pdfReader.numPages
+print(pages)
+
+speaker=pyttsx3.init()
+
+for num in range(4,pages):
+    page=pdfReader.getPage(num)
+    text=page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
